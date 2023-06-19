224c3d7cb6783d00f0d37af6f15a0eaab5f0638f
Aravind S <127675034+AravindSaravananGitHub@users.noreply.github.com>
2023-06-20 00:48:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const kits = ["crash", "kick", "snare", "tom"];
+const container = document.querySelector(".container");
+
+kits.forEach((kit) => {
+  //console.log(kit);
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.style.backgroundImage = `url(image/${kit}.png)`;
+  btn.innerText = kit;
+  container.appendChild(btn);
+  const audio = document.createElement("audio");
+  audio.src = `sound/edited${kit}.mpeg`;
+  container.appendChild(audio);
+
+  btn.addEventListener("click", () => {
+    audio.play();
+  });
+
+  window.addEventListener("keydown", (e) => {
+    //console.log(e);
+    if (e.key === kit.slice(0, 1)) {
+      audio.play();
+      btn.style.transform = "scale(.9)";
+      setTimeout(() => {
+        btn.style.transform = "scale(1)";
+      }, 100);
+    }
+  });
+});
